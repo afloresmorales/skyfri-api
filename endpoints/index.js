dc8f5a1066374a878e87d2b50b1d3efde648e4bb
afloresmorales <afloresmorales96@gmail.com>
2022-11-05 00:40:26 -0600

    adding two endpoints for users

--- endpoints/index.js
+++ endpoints/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const endpoints = express.Router();
 const mongoDb = require('../db/config');
+const crypto = require('crypto'); 
 
 endpoints.route('/agreements').post(async function (req, res) {
     const db = mongoDb.getDb();
     console.log({body: req.body})
     const document = {
-      agreement: req.body.agreement,
+      agreement,
       billing:req.body.billing,
       subsidiary: req.body.subsidiary,
       payment: req.body.payment,
@@ -14,9 +15,8 @@ endpoints.route('/agreements').post(async function (req, res) {
       startDate: req.body.startDate,
       endDate: req.body.endDate
     };
-    const col = db.collection("agreements");
-    const result = await col.insertOne(document);
-    console.log({result})
+    const collection = db.collection("agreements");
+    const result = await collection.insertOne(document);
     if(result.acknowledged){
         res.status(200).send();
     } else {
@@ -25,20 +25,36 @@ endpoints.route('/agreements').post(async function (req, res) {
   });
   endpoints.route('/users').post(async function (req, res) {
     const db = mongoDb.getDb();
-    console.log({body: req.body})
+    const salt = crypto.randomBytes(16).toString('hex'); 
+    const {username, email, password, fullName} = req.body;
+    const hashedPassword = crypto.pbkdf2Sync(password, salt,  
+    1000, 64, `sha512`).toString(`hex`); 
     const document = {
-      username: req.body.username,
-      email:req.body.email,
-      password: req.body.password,
-      fullName: req.body.fullName,
+      username,
+      email,
+      fullName,
+      password: hashedPassword,
     };
-    const col = db.collection("users");
-    const result = await col.insertOne(document);
+    const collection = db.collection("users");
+    const result = await collection.insertOne(document);
     console.log({result})
     if(result.acknowledged){
         res.status(200).send();
     } else {
-        res.status(400).send('Unable to insert agreement')
+        res.status(400).send('Unable to create user')
+    }
+  });
+  endpoints.route('/users').get(async function (req, res) {
+    const db = mongoDb.getDb();
+
+     const {username} = req.query;
+    
+    const collection = db.collection("users");
+    const result = await collection.findOne({username});
+    if(result){
+        res.status(200).json(result);
+    } else {
+        res.status(400).send('Unable to create user')
     }
   });
   module.exports = endpoints;
